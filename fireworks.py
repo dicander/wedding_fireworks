03618b8c1c4e8bb5d3a65b84d4cff01d17c4e506
Marcus Dicander <dicander@kth.se>
2024-06-30 21:42:22 +0200

    Fixed colors to be more fireworks-like

--- fireworks.py
+++ fireworks.py
@@ -2,10 +2,6 @@ import pygame
 import random
 import math
 
-# 4k display
-SCREEN_WIDTH = 1920
-SCREEN_HEIGHT = 1080
-ASPECT_RATIO = SCREEN_WIDTH / SCREEN_HEIGHT
 N_PARTICLES = 10000
 EXPLOSION_SIZE_MIN = 50
 EXPLOSION_SIZE_MAX = 700
@@ -33,11 +29,11 @@ class Particle:
         self.x += self.dx
         self.y += self.dy
         self.dy += 0.0005
-        self.dx *= 0.99
+        self.dx *= 0.999
 
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, 
-                           (project(self.x, SCREEN_WIDTH), project(self.y, SCREEN_HEIGHT)), 
+                           (project(self.x, width/aspect), project(self.y, height)), 
                            self.size)
 
 
@@ -45,7 +41,7 @@ def resurrect_for_fireworks():
     """This function resurrects particles for fireworks. It picks fireworks that are below the screen and moves
     them close to where the fireworks are launched."""
     explosion_size = random.randint(EXPLOSION_SIZE_MIN, EXPLOSION_SIZE_MAX)
-    explosion_center_x = random.uniform(0.0, 1.0)
+    explosion_center_x = random.uniform(0.0, aspect)
     explosion_center_y = random.uniform(0.0, 1.0)
     for particle in particles:
         """Note that particles x,y,dx and dy are floats."""
@@ -53,15 +49,12 @@ def resurrect_for_fireworks():
             angle = random.uniform(0, 2 * 3.14159)
             distance = random.uniform(0, 0.01)
             particle.x = explosion_center_x + distance * math.cos(angle) 
-            particle.y = explosion_center_y + distance * math.sin(angle) / ASPECT_RATIO
+            particle.y = explosion_center_y + distance * math.sin(angle)
             launch_angle = random.uniform(0, 2 * 3.14159)
             launch_speed = random.uniform(0.0, 1.0/60)
             particle.dx = launch_speed * math.cos(launch_angle)
             particle.dy = launch_speed * math.sin(launch_angle)
             particle.size = random.randint(1, 5)
-            particle.color = pygame.Color(random.randint(1, 255),
-                                          random.randint(1, 255),
-                                          random.randint(1, 255))
             explosion_size -= 1
             if explosion_size == 0:
                 return
@@ -70,16 +63,18 @@ def resurrect_for_fireworks():
 
 def init():
     for i in range(N_PARTICLES):
-        particles.append(Particle(2.0, 1.1, 0, 0, pygame.Color(random.randint(1, 255),
-                                                           random.randint(1, 255),
-                                                            random.randint(1, 255)) , 0))
+        yellow = random.randint(0, 255)
+        particles.append(Particle(2.0, 1.1, 0, 0, pygame.Color(random.randint(yellow, 255),
+                                                           yellow,
+                                                           0) , 0))
 
 def main():
     pygame.init()
-    # set screen mode to 1080p
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-#    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
- #   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    global width, height, aspect
+    width = screen.get_width()
+    height = screen.get_height()
+    aspect = width / height
     clock = pygame.time.Clock()
     init()
     while True:
@@ -95,11 +90,12 @@ def main():
         for particle in particles:
             particle.move()
             particle.draw(screen)
-        # Resurrect particles with 10% probability
+        # Resurrect particles to be part of a new firework
         if random.random() < 0.05:
             resurrect_for_fireworks()
+        clock.tick_busy_loop(60)
         pygame.display.flip()
-        clock.tick(60)
+        
 
 
 if __name__ == "__main__":
